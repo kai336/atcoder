6afb6b895ff596aae0ed2cce410c00e9f1ee6909
kai336 <shiturukai.336@gmail.com>
2023-06-20 03:22:41 +0900

    tmp

--- ABC049C.c
+++ ABC049C.c
@@ -10,11 +10,54 @@ int main(void){
     char string[MAX_SIZE];
     scanf("%s", string);
     //("%s\n", string);
-    char buff[20];
+    char buff[10];
+    char buff2[10];
     
-    int tmp = strlen(string);
+    int len = strlen(string);
+    int mark = len - 1;
 
-    printf("%d\n", tmp);
+    if(len < 5){
+        printf("NO\n");
+        return 0;
+    }
+
+    int i, flag;
+    flag = 0;
+    switch(string[mark]){
+        case 'm':
+            for(i=0; i<5; i++){
+                buff[i] = string[mark - 4 + i];
+            }
+            //printf("%s\n", buff);
+            if(strncmp(buff, token[0], 5)==0){
+                mark = mark - 5;
+                /*if(mark < 5){
+                    printf("NO\n");
+                    return 0;
+                }*/
+            }else flag = 1;
+            
+            break;
+
+        case 'r':
+            for(i=0; i<7; i++){
+                buff[i] = string[mark - 6 + i];
+            }
+            //printf("%s\n", buff);
+            if(strncmp(buff, token[1], 7)==0){
+                mark = mark - 7;
+            }else flag = 1;
+
+            
+
+            break;
+            
+        case 'e':
+            break;
+
+        default:
+            printf("NO\n");
+    }
 
     return 0;
 }
