1636d68f794819af2a30b37f566dffa8c6035687
kai336 <shiturukai.336@gmail.com>
2023-06-20 12:57:25 +0900

    roop nasi ver

--- ABC049C.c
+++ ABC049C.c
@@ -6,7 +6,7 @@
 
 int main(void){
     char token[4][10]= { "dream", "dreamer", "erase", "eraser" };
-    char alphabet[6][2] = {"a", "d", "e", "m", "r", "s"};
+    //char alphabet[6][2] = {"a", "d", "e", "m", "r", "s"};
     char string[MAX_SIZE];
     scanf("%s", string);
     //("%s\n", string);
@@ -28,35 +28,72 @@ int main(void){
             for(i=0; i<5; i++){
                 buff[i] = string[mark - 4 + i];
             }
-            //printf("%s\n", buff);
+            printf("%s\n", buff);
             if(strncmp(buff, token[0], 5)==0){
                 mark = mark - 5;
-                /*if(mark < 5){
-                    printf("NO\n");
-                    return 0;
+
+                /*for(i=0; i<5; i++){
+                    buff[i] = NULL;
                 }*/
+
             }else flag = 1;
             
             break;
 
         case 'r':
-            for(i=0; i<7; i++){
-                buff[i] = string[mark - 6 + i];
-            }
-            //printf("%s\n", buff);
-            if(strncmp(buff, token[1], 7)==0){
-                mark = mark - 7;
-            }else flag = 1;
+            switch(string[mark - 2]){
+                case 'm':
+                    for(i=0; i<7; i++){
+                        buff[i] = string[mark - 6 + i];
+                    }
+                    printf("%s\n", buff);
+                    if(strncmp(buff, token[1], 6)==0){
+                        mark = mark - 6;
 
-            
+                        /*for(i=0; i<7; i++){
+                            buff[i] = NULL;
+                        }*/
+
+                    }else flag = 1;
+
+                    break;
+
+                case 's':
+                    for(i=0; i<6; i++){
+                        buff[i] = string[mark - 5 + i];
+                    }
+                    printf("%s\n", buff);
+                    if(strncmp(buff, token[3], 6)==0){
+                        mark = mark - 6;
+
+                        /*for(i=0; i<6; i++){
+                            buff[i] = NULL;
+                        }*/
+
+                    }else flag = 1;
+
+                    break;
+            }
 
             break;
             
         case 'e':
+            for(i=0; i<5; i++){
+                buff[i] = string[mark - 4 + i];
+            }
+            printf("%s\n", buff);
+            if(strncmp(buff, token[2], 5)==0){
+                mark = mark - 5;
+            }else flag = 1;
+
             break;
 
         default:
             printf("NO\n");
+    }
+
+    if(flag == 1){
+        printf("NO\n");
     }
 
     return 0;
