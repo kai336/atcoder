945f4206585c0eefa50ee876bc9eae7b9e267154
kai336 <shiturukai.336@gmail.com>
2023-06-21 02:44:52 +0900

    nyuuryokudake

--- tenkei90/001.c
+++ tenkei90/001.c
@@ -2,3 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+int main(void){
+    int n, l, k;
+
+    scanf("%d%d", &n, &l);
+    scanf("%d", &k);
+
+    int a[n+2];
+    a[0] = 0;
+    a[n+1] = l;
+
+    char buff[1000000];
+
+    char gomi = getchar();
+
+    fgets(buff, sizeof(buff), stdin);
+
+    int i;
+    char *token;
+    token = strtok(buff, " \n");
+    for(i=1; i<n+1; i++){
+        a[i] = atoi(token);
+        token = strtok(NULL, " \n");
+    }
+
+    return 0;
+}
