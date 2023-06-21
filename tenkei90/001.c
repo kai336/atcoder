8be46b066766ce954d0be8e3761c04e854f0b3ba
kai336 <shiturukai.336@gmail.com>
2023-06-21 13:13:54 +0900

    ac

--- tenkei90/001.c
+++ tenkei90/001.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+bool check(int x, int n, int k, int *a){
+    int count, last, i;
+    count = 0;
+    last = 0;
+    for(i=1; i<n+2; i++){
+        if(a[i] - last >= x){
+            last = a[i];
+            count++;
+        }
+    }
+    if(count < k + 1){
+        return false;
+    }else return true;
+}
 
 int main(void){
     int n, l, k;
@@ -18,13 +34,28 @@ int main(void){
 
     fgets(buff, sizeof(buff), stdin);
 
-    int i;
+    int i, j;
     char *token;
     token = strtok(buff, " \n");
     for(i=1; i<n+1; i++){
         a[i] = atoi(token);
         token = strtok(NULL, " \n");
     }
+
+    int left, mid, right;
+    left = -1;
+    right = a[n+1] + 1;
+    
+    //binary search
+    bool c;
+    while(left + 1 < right){
+        mid = (left + right) / 2;
+        c = check(mid, n, k, a);
+        if(c) left = mid;
+        else right = mid;
+    }
+
+    printf("%d\n", left);
 
     return 0;
 }
