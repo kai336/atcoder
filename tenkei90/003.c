971c093b15f23203213c95b129897ff2f6ec451c
kai336 <shiturukai.336@gmail.com>
2023-06-23 01:35:19 +0900

    adjlist deyokatta dfs totyu

--- tenkei90/003.c
+++ tenkei90/003.c
@@ -4,19 +4,58 @@
 #include <stdbool.h>
 #include <math.h>
 
-void *makeadjmatrix(int n, int graph[n-1][2], int adjmatrix[n][n]){
-    int i, index_start, index_goal;
-    for(i=0; i<n-1; i++){
-        index_start = graph[i][0] - 1;
-        index_goal = graph[i][1] - 1;
-        adjmatrix[index_start][index_goal] = 1;
-        adjmatrix[index_goal][index_start] = 1;
-        //printf("adj+ %d %d\n", index_start, index_goal);
+#define MAX 100000
+
+typedef struct{
+    int data[MAX];
+    int top;
+}stack;
+
+void stackpush(int x, stack stack){
+    stack.data[stack.top] = x;
+    stack.top++;
+}
+
+void stackpop(stack stack){
+    stack.data[stack.top] = 0;
+    stack.top--;
+}
+
+void stackinit(stack stack){
+    int i;
+    stack.top = 0;
+    for(i=0; i<MAX; i++){
+        stack.data[i] = 0;
     }
 }
 
-int countscore(int n, int adjmatrix[n][n]){
+void makeadjlist(int n, int graph[n-1][2], int adjlist[n-1][n-1]){
+    int i, index_start, goal;
+    int top[n-1];
+    for(i=0; i<n-1; i++){
+        top[i] = 0;
+    }
+    for(i=0; i<n-1; i++){
+        index_start = graph[i][0] - 1;
+        goal = graph[i][1] - 1;
+        adjlist[index_start][top[index_start]] = goal;
+        printf("%d %d %d\n", index_start, top[index_start], adjlist[index_start][top[index_start]]);
+        top[index_start]++;
+    }
+}
+
+int countscore(int n, int adjlist[n][n]){
     int i, j;
+    stack stack;
+    stackinit(stack);
+    bool notend = true;
+    bool notempty = true;    
+    i = 0;
+
+    //find deepest vertex from 0
+    while(notend && notempty){
+        
+    }
 
 }
 
@@ -25,12 +64,12 @@ int main(void){
     scanf("%d", &n);
     
     int graph[n-1][2];
-    int adjmatrix[n][n];
+    int adjlist[n-1][n-1];
 
     //init adjlist
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
-            adjmatrix[i][j] = 0;
+    for(i=0; i<n-1; i++){
+        for(j=0; j<n-1; j++){
+            adjlist[i][j] = 0;
         }
     }
 
@@ -46,14 +85,16 @@ int main(void){
     */
 
     //make adjlist
-    makeadjmatrix(n, graph, adjmatrix);
+    makeadjlist(n, graph, adjlist);
 
-    for(i=0; i<n; i++){
-        for(j=0; j<n; j++){
-            printf("%d ", adjmatrix[i][j]);
+    for(i=0; i<n-1; i++){
+        for(j=0; j<n-1; j++){
+            printf("%d ", adjlist[i][j]);
         }
         printf("\n");
     }
-    
+
+    //find diamater
+
     return 0;
 }
